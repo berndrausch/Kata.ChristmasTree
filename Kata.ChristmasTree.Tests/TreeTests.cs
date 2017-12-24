81cc020d14248cc6c6569dbd215641aa7cae22ad
berndrausch <bernd@rauschpost.de>
2017-12-24 11:19:42 +0100

    More tests

--- Kata.ChristmasTree.Tests/TreeTests.cs
+++ Kata.ChristmasTree.Tests/TreeTests.cs
@@ -33,5 +33,38 @@ namespace Kata.ChristmasTree.Tests
             Assert.AreEqual("X", actual[1]);
             Assert.AreEqual("I", actual[2]);           
         }
+
+        [TestMethod]
+        public void ComplexTreeWithoutStar()
+        {
+            Tree sut = new Tree();
+
+            List<String> actual = sut.GetLines(5, false).ToList();
+
+            Assert.AreEqual(6, actual.Count);
+            Assert.AreEqual("    X", actual[0]);
+            Assert.AreEqual("   XXX", actual[1]);
+            Assert.AreEqual("  XXXXX", actual[2]);
+            Assert.AreEqual(" XXXXXXX", actual[3]);
+            Assert.AreEqual("XXXXXXXXX", actual[4]);
+            Assert.AreEqual("    I", actual[5]);           
+        }
+
+        [TestMethod]
+        public void ComplexTreeWithStar()
+        {
+            Tree sut = new Tree();
+
+            List<String> actual = sut.GetLines(5, true).ToList();
+
+            Assert.AreEqual(7, actual.Count);
+            Assert.AreEqual("    *", actual[0]);
+            Assert.AreEqual("    X", actual[1]);
+            Assert.AreEqual("   XXX", actual[2]);
+            Assert.AreEqual("  XXXXX", actual[3]);
+            Assert.AreEqual(" XXXXXXX", actual[4]);
+            Assert.AreEqual("XXXXXXXXX", actual[5]);
+            Assert.AreEqual("    I", actual[6]);           
+        }
     }
 }
